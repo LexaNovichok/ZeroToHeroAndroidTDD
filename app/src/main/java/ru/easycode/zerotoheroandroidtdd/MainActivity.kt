c001d3a14762c77e8d695161310e76a78579ab6b
Little_Indian_Dev <alexzaitsev04@mail.ru>
2024-09-07 09:19:27 +0300

    task/007 solved onSaveInstanceState

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,47 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var text : TextView
+
+    private val KEY = "123"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.changeButton)
+        text = findViewById(R.id.titleTextView)
+
+        var newText = savedInstanceState?.getString(KEY)
+        if (newText != null) {
+            text.setText(newText)
+        }
+
+
+
+        button.setOnClickListener {
+            text.setText("I am an Android Developer!")
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(KEY, text.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        var newText = savedInstanceState?.getString(KEY)
+        if (newText != null) {
+            text.setText(newText)
+        }
+    }
+
+
 }
