4e147e6c3c25ea03a3bcf63c96da1983ebe7ceda
Little_Indian_Dev <alexzaitsev04@mail.ru>
2024-09-08 11:48:12 +0300

    task/009 solved OOP

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,18 +1,21 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
+    private var state : State = State.Initial
 
     private lateinit var buttonRemove : Button
     private lateinit var text : TextView
     private lateinit var linearLayout : LinearLayout
-    private var isExist = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,24 +26,46 @@ class MainActivity : AppCompatActivity() {
         linearLayout = findViewById(R.id.rootLayout)
 
         buttonRemove.setOnClickListener {
-            linearLayout.removeView(text)
-            isExist = false
+            state = State.Removed
+            state.apply(linearLayout, text)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        outState.putBoolean("KEY", isExist)
+        outState.putSerializable(KEY, state)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
-        if (!savedInstanceState.getBoolean("KEY")) {
-            linearLayout.removeView(text)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            state = savedInstanceState.getSerializable(KEY, State::class.java) as State
+        } else {
+            state = savedInstanceState.getSerializable(KEY) as State
+        }
+        state.apply(linearLayout, text)
+    }
 
-            //isExist = false
+    companion object {
+        private const val KEY = "KEY"
+    }
+}
+
+
+interface State : Serializable {
+
+    fun apply(linearLayout: LinearLayout, textView: TextView)
+    object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView) = Unit
+
+    }
+
+    object Removed : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView) {
+            linearLayout.removeView(textView)
         }
     }
 }
+
