ae075a42bc6b17598ca48237c44fa5af5069e68f
Little_Indian_Dev <alexzaitsev04@mail.ru>
2024-10-03 00:02:46 +0300

    task/021 solved
    addTextChangedListener for editTextView

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,27 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.inputEditText.addTextChangedListener {
+            binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
+        }
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+
+            binding.titleTextView.text = text
+            binding.inputEditText.setText("")
+        }
     }
 }
