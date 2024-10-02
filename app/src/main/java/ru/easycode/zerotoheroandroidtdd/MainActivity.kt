639813678f10fd83624661abb0870e5cc88a6437
Little_Indian_Dev <alexzaitsev04@mail.ru>
2024-10-02 23:38:33 +0300

    task/020 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,23 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            binding.titleTextView.text = text
+            binding.inputEditText.setText("")
+            Log.d("LALALA", "Text: $text")
+        }
     }
 }
