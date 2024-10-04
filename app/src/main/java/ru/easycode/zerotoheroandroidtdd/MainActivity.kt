a01dd61a84e274d2a2d9f11d11714bf8ed0b51ba
Little_Indian_Dev <alexzaitsev04@mail.ru>
2024-10-04 21:19:02 +0300

    task/022 solved easy way

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,47 +1,54 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private lateinit var viewModel : MainViewModel
+
+    private val textList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val viewModel = (application as App).viewModel
-
         binding.actionButton.setOnClickListener {
-            val addedText = binding.inputEditText.text.toString()
+            val currentText = binding.inputEditText.text.toString()
 
-            viewModel.createTextView(this, addedText)
+            textList.add(currentText)
+            addItem(currentText)
+            binding.inputEditText.setText("")
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        viewModel.save(BundleWrapper.Base(outState))
-
+        outState.putStringArrayList(KEY, textList)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        viewModel.restore(BundleWrapper.Base(savedInstanceState))
+        val list = savedInstanceState.getStringArrayList(KEY)?: ArrayList()
+        textList.addAll(list)
 
-
-        val list = viewModel.liveData().value
-
-        if (list != null) {
-            for (itemText in list) {
-                binding.contentLayout.addView(viewModel.createTextView(this, itemText))
-            }
+        list.forEach { item ->
+            addItem(item)
         }
     }
 
+    private fun addItem(text : String) {
+        val textView = TextView(this)
+        textView.text = text
+        textView.textSize = 22f
+
+        binding.contentLayout.addView(textView)
+    }
+
+    companion object {
+        private const val KEY = "KEY"
+    }
 }
